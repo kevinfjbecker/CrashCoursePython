191d915c93b247a5c446b6af9be3c645c4e33142
Kevin Becker <kevinfjbecker@gmail.com>
2018-05-10 15:32:14 +0200

    Start of chapter 13

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,12 @@
-import sys
 import pygame
+from pygame.sprite import Group
+
 from settings import Settings
 from ship import Ship
 import game_functions as gf
 
-
 def run_game():
-    # Initialze game and create a screen object.
+    # Initialize pygame, settings, and screen object.
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
@@ -14,13 +14,15 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # Make a ship.
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
-
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
